8d969a79c0196185b471b55eea23bd327522773d
songjihu <275245802@qq.com>
2018-12-01 21:49:47 +0800

    提取产生式，终结符，非终结符

--- test/test.c
+++ test/test.c
@@ -25,6 +25,8 @@ char allSymbol[400];     //所有符号
 char firstSET[100][100]; //各产生式的右部FIRST集
 char followSET[100][100];//各产生式的右部FOLLOW集 
 
+int g_num = 0, t_num = 0, nt_num = 0;//3种要存入的个数,最后一个内容有重复
+
 int M[200][200];         //分析表 
 
 void pp(Stack *s, Stack *s1)
@@ -67,7 +69,8 @@ int main()
 	err = fopen_s(&fp,"input.txt","r");
 
 	//1.首先确认可以打开input.txt;
-	if (fp == NULL) {
+	if (fp == NULL) 
+	{
 		printf("无法打开!\n");
 		return 0;
 	}
@@ -83,61 +86,135 @@ int information(FILE*fp)//初始化代码并入栈。
 {
 	//printf("hou s = %d\n",s);
 	int flag = 0;
-	int i = 0, j = 0, fact = 0, t = 0, m = 0;
-	int g_num = 0,t_num = 0,nt_num = 0;//3种要存入的个数
+	int i = 0, j = 0, fact = 0, t = 0, m = 0;//m为非终结符循环控制参数
+	
 	char *temp = 0;
 	char over;
 	temp = malloc(10000);
 	fseek(fp, 0, SEEK_SET);//定位 
 	fread(temp, sizeof(char), 10000, fp);
 	//1.处理产生式
-	for (i = 0; i<1000; i++){
+	for (i = 0; i<1000; i++)
+	{
 		//跳出 
-		if (temp[i-1] == 10){
+		if (temp[i-1] == 10)
+		{
 			break;
 		}
 		//未结束存入grammarElement gramOldSet[200];char terSymbol[200];char non_ter[200];
-		else{
+		else
+		{
 			printf("**");
-			for (t = 0; t < 10; t++) {
+			for (t = 0; t < 10; t++) 
+			{
 				
-				if (temp[i] == 32|| temp[i] == 10) {
-					
+				if (temp[i] == 32|| temp[i] == 10)
+				{
+					gramOldSet[g_num].formula[20] = t;//保存产生式长度在20的位置
 					g_num++;
-					j = t;
+				
 					break;
 				}
-				gramOldSet[g_num].formula[t] = temp[i];
-				printf("%c", gramOldSet[g_num].formula[t] = temp[i]);
+				
+				
+				
+				//转换
+				if (temp[i+1] == '\'')
+				{
+					//带'的大写换小写，用e代表E'
+					gramOldSet[g_num].formula[t] = temp[i];
+					gramOldSet[g_num].formula[t] = gramOldSet[g_num].formula[t] + 32;
+					i++;
+				}
+				else
+				{
+					//替换 易普逊 为 @
+					if (temp[i] == -90)
+					{
+						gramOldSet[g_num].formula[t] = 64;
+						i++;
+					}
+					else
+					{
+						//替换 箭头 为 > 
+						if (temp[i] == -95)
+						{
+							gramOldSet[g_num].formula[t] = '>';
+							i++;
+						}
+						else
+						{
+							gramOldSet[g_num].formula[t] = temp[i];
+						}
+						
+					}
+					
+				}
+				printf("%c", gramOldSet[g_num].formula[t]);
 				i++;
 
 			}
 			printf("**\n");
 		}
-		//2.处理非终结符
-		for (i=0; i<1000; i++) {
-			//跳出 
-			if (temp[i] == '10') {
+		
+			//未结束存入grammarElement gramOldSet[200];char terSymbol[200];char non_ter[200];
+		
+	}
+
+	//2.处理终结符
+	for (t = 0; t < g_num; t++)
+	{
+		printf("++");
+		terSymbol[t] = gramOldSet[t].formula[0];
+        printf("%c", terSymbol[t]);
+		printf("++\n");
+
+	}
+	t_num = g_num;
+
+	//3.处理非终结符
+	for (t = 0; t < g_num; t++)
+	{
+
+		//取箭头后面的字符
+		for (i = 0; i < gramOldSet[t].formula[20]; i++)
+		{
+			if (gramOldSet[t].formula[i] == '>')
+			{
+				i=i+1;
+				//printf("hhah");
 				break;
 			}
-			//未结束存入grammarElement gramOldSet[200];char terSymbol[200];char non_ter[200];
-			else {
-				for (t = 0; t < 10; t++) {
-					if (temp[i] == ' ') {
-						break;
-					}
-					gramOldSet[j].formula[t] = temp[i];
-					i++;
-
+		}
+		//得到非终结符
+		for (; i < gramOldSet[t].formula[20]; i++)
+		{
+			//若是终结符或者'|'
+			flag = 0;
+			for (j = 0; j < g_num; j++)
+			{
+				if (gramOldSet[t].formula[i] == terSymbol[j]|| gramOldSet[t].formula[i] == '|')
+				{
+					flag=1;
+					break;
 				}
-				printf("%c==%d  ", temp[i], temp[i]);
-				j++;
-
 			}
+			if (flag==1)
+			{
+				continue;
+			}
+			//若不是则存入
+			non_ter[m] = gramOldSet[t].formula[i];
+			
+			printf("&&");
+			printf("%c", non_ter[m]);
+			printf("&&\n");
+			m++;
+		}
 	}
-	fact = g_num;
+	nt_num = m;
 
-	printf("HHHHH%d\n", g_num);
+	printf("%d,%d,%d\n", g_num,t_num,nt_num);
 	return fact;
 }
 
